d8dea983ffe312de155055a8104976b2c589f3d2
Asif Sayyed <asifdotexe@gmail.com>
2024-11-08 00:02:45 +0530

    #7 created the handle missing value module

--- quickvu/data_prepper.py
+++ quickvu/data_prepper.py
@@ -17,3 +17,25 @@ def get_data_overview(dataframe: pd.DataFrame) -> dict:
         "sample_rows": dataframe.head().to_dict(orient="records"),
     }
     return data_overview
+
+def handle_missing_values(dataframe: pd.DataFrame, method: str = "drop") -> pd.DataFrame:
+    """Handle missing values in the dataset based on the chosen method.
+    
+    :param dataframe: The Dataframe to handle missing values for.
+    :type dataframe: pd.DataFrame
+    :param method: The method handle missing values. Options are `drop`, `mean`, or `median`.
+    :type method: str, optional
+        Default is `drop`
+    
+    :return: The DataFrame with missing values handled.
+    :rtype: pd.DataFrame
+    """
+    if method == "drop":
+        dataframe = dataframe.dropna()
+    elif method == "mean":
+        dataframe = dataframe.fillna(dataframe.mean())
+    elif method == "median":
+        dataframe = dataframe.fillna(dataframe.median())
+    else:
+        raise ValueError("Invalid method for handling missing values. Choose 'drop', 'mean', or 'median'.")
+    return dataframe
