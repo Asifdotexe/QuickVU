4ee07f487eb8f237be83fda8859fe4e643d03957
Asif Sayyed <asifdotexe@gmail.com>
2025-01-08 14:33:58 +0530

    #29 updated the formatting of quickModelSelection.py

--- app_pages/quickModelSelection.py
+++ app_pages/quickModelSelection.py
@@ -4,10 +4,13 @@ from quickvu import gemini
 with open('app_pages/styles.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-st.markdown('<h1 class="main-header">🔍 Quick Select: Model Selection</h1>', unsafe_allow_html=True)
+st.markdown('<h1 class="main-header">🔍 Quick Select: Model Selection</h1>',
+            unsafe_allow_html=True)
 
 st.markdown("""
-Quick Glance is a data analysis tool that provides summary statistics, visualizes correlations, and generates quick plots to give you a better understanding of your data.
+Quick Glance is a data analysis tool that provides summary statistics, 
+visualizes correlations, and generates quick plots to give you a better 
+understanding of your data.
 """)
 
 st.sidebar.image('./dataset/logo-png.png', use_container_width=True)
