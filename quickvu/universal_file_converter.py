688c0cde078f5fc116ae8f366a811a3091e3a25a
Asif Sayyed <asifdotexe@gmail.com>
2025-02-04 14:13:09 +0530

    #30 coded the module for file converter

--- quickvu/universal_file_converter.py
+++ quickvu/universal_file_converter.py
@@ -3,19 +3,16 @@ This script is for a universal file converter, that can convert any file
 into any other compatible type of file
 """
 
-import os
-import json
 import pandas as pd
-import pyarrow.parquet as pq
 
-def detect_file_type(file_name: str) -> str:
+def detect_file_type(file_name: str) -> tuple[str, str]:
     """This function takes the file name and extracts the extension
 
     :param file_name: name of the input file
     :type file_name: str
     """
-    extension = file_name.split('.')[-1].lower()
-    return extension
+    current_file_name, extension = file_name.split('.')
+    return current_file_name, extension.lower()
 
 def read_file(uploaded_file, file_type):
     """This function reads the input file and based on the datatype
@@ -43,3 +40,19 @@ def get_conversion_options(file_type: str):
         "parquet": ["csv", "xlsx"]
     }
     return options.get(file_type, [])
+
+def convert_file(df: pd.DataFrame, convert_to: str, current_file_name: str):
+    """
+    This function is responsible for changing the datatype to the one specified in
+    `convert_to` parameter
+    """
+    output_path = f"converted_{current_file_name}.{convert_to}"
+    if convert_to == "csv":
+        df.to_csv(output_path, index=False)
+    elif convert_to == "xlsx":
+        df.to_excel(output_path, index=False)
+    elif convert_to == "json":
+        df.to_json(output_path, orient="records", indent=2)
+    elif convert_to == "parquet":
+        df.to_parquet(output_path)
+    return output_path
