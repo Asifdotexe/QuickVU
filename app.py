14e1d5c6323de6e717b53ebb6caa89774c2cbebc
Asif Sayyed <asifdotexe@gmail.com>
2025-02-04 14:11:10 +0530

    #30 added quick converter under the tools tab

--- app.py
+++ app.py
@@ -3,14 +3,31 @@ import streamlit as st
 # Define your pages using st.Page objects
 pages = {
     "Main Pages": [
-        st.Page("app_pages/home.py", title="Home", icon=":material/home:"),
+        st.Page("app_pages/home.py",
+                title="Home",
+                icon=":material/home:"),
     ],
+
     "Apps": [
-        st.Page("app_pages/quickPrep.py", title="Quick Prep: Data Cleaning", icon=":material/mop:"),
-        st.Page("app_pages/quickGlance.py", title="Quick Glance: Data Analysis", icon=":material/search:",),
+        st.Page("app_pages/quickPrep.py",
+                title="Quick Prep: Data Cleaning",
+                icon=":material/mop:"),
+        st.Page("app_pages/quickGlance.py",
+                title="Quick Glance: Data Analysis",
+                icon=":material/search:",),
     ],
+
+    "Tools": [
+        st.Page("app_pages/quickConverter.py",
+                title="Quick Converter",
+                icon=":material/compare_arrows:"),
+        # st.Page("app_pages/quickGlance.py", title="Quick Glance: Data Analysis", icon=":material/search:",),
+    ],
+
     "Others": {
-        st.Page("app_pages/quickFeedback.py", title="Feedback", icon=":material/rate_review:")
+        st.Page("app_pages/quickFeedback.py",
+                title="Feedback",
+                icon=":material/rate_review:")
     }
 }
 
