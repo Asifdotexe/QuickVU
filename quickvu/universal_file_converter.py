151dbc51b177255eb2babdbd8140dac029f46c6e
Asif Sayyed <asifdotexe@gmail.com>
2025-02-04 13:06:14 +0530

    #30 wrote a function to read file

--- quickvu/universal_file_converter.py
+++ quickvu/universal_file_converter.py
@@ -17,4 +17,17 @@ def detect_file_type(file_name: str) -> str:
     extension = file_name.split('.')[-1].lower()
     return extension
 
-
+def read_file(uploaded_file, file_type):
+    """This function reads the input file and based on the datatype
+    reads the file into a dataframe
+    """
+    if file_type == "csv":
+        return pd.read_csv(uploaded_file)
+    elif file_type in ["xls", "xlsx"]:
+        return pd.read_excel(uploaded_file)
+    elif file_type == "json":
+        return pd.read_json(uploaded_file, orient="records")
+    elif file_type == "parquet":
+        return pd.read_parquet(uploaded_file)
+    else:
+        return None
